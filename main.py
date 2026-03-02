b758569cf5c53415c035edc0f76c7527b6156810
liovirgilda <liovirgilda.mendonca@gmail.com>
2026-03-02 19:15:44 +0000

    Create layout.html page for template inheritance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -8,6 +7,6 @@ templates = Jinja2Templates(directory = "templates")
 
 # HTML routes
 
-@app.get("/",  response_class = HTMLResponse, include_in_schema = False)
+@app.get("/", include_in_schema = False)
 def homepage(request:Request):
     return templates.TemplateResponse(request, "homepage.html", {"title": "Homepage"})
