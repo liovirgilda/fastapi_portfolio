7f4f4628f6263feb24e27754fae92d2ee2a9de34
liovirgilda <liovirgilda.mendonca@gmail.com>
2026-03-03 18:47:19 +0000

    Add navbar dark mode button

--- static/js/main.js
+++ static/js/main.js
@@ -11,6 +11,25 @@ const navbar_typing = new Typed('#typing-text', {
     loop: true
   });
 
+// Dark mode button
+
+const toggleBtn = document.getElementById("theme-toggle");
+const html = document.documentElement;
+
+toggleBtn.addEventListener("click", () => {
+
+  const currentTheme = html.getAttribute("data-bs-theme");
+
+  if (currentTheme === "light") {
+    html.setAttribute("data-bs-theme", "dark");
+    toggleBtn.textContent = "☀️ Light";
+  } else {
+    html.setAttribute("data-bs-theme", "light");
+    toggleBtn.textContent = "🌚 Dark";
+  }
+
+});
+
 // Footer copyright year 
 
 document.getElementById('year').textContent = new Date().getFullYear();
