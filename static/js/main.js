b7a1be91b704bb54c2f3783e30607e860e579ba6
liovirgilda <liovirgilda.mendonca@gmail.com>
2026-03-04 22:21:37 +0000

    Add homepage background img, remove typing message, create snipped.txt file

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,3 @@
-// Typed navbar message
-
-const navbar_typing = new Typed('#typing-text', {
-    strings: [
-      "Welcome to my website",
-      "A Software Developer by heart",
-      "A Data Analyst Expert"
-    ],
-    typeSpeed: 110,
-    backSpeed: 50,
-    loop: true
-  });
-
 // Dark mode button
 
 const toggleBtn = document.getElementById("theme-toggle");
